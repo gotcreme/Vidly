01fad499ed2db02beebe56ef52faf24193491cc0
Volodymyr Dykun <Volodymyr Dykun@DESKTOP-DM79T2C>
2017-02-06 16:44:18 +0200

    movie form validation

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -60,18 +60,28 @@ namespace Vidly.Controllers
             if (movie == null)
                 return HttpNotFound();
 
-            var viewModel = new MovieFormViewModel
-            {
+            var viewModel = new MovieFormViewModel(movie)
+            {              
                 Genres = _context.Genres.ToList(),
-                Movie = movie
             };
 
             return View("MovieForm", viewModel);
         }
 
         [HttpPost]
+        [ValidateAntiForgeryToken]
         public ActionResult Save(Movie movie)
         {
+            if (!ModelState.IsValid)
+            {
+                var viewModel = new MovieFormViewModel(movie)
+                {
+                    Genres = _context.Genres.ToList()
+                };
+
+                return View("MovieForm", viewModel);
+            }
+
             if (movie.Id == 0)
              {
                  movie.DateAdded = DateTime.Now;
