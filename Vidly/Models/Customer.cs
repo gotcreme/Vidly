04c61eb57222e6a05df8bd9ce56bc4c2e9526456
Volodymyr Dykun <Volodymyr Dykun@DESKTOP-DM79T2C>
2017-02-06 16:07:15 +0200

    Add validation to a customer form

--- Vidly/Models/Customer.cs
+++ Vidly/Models/Customer.cs
@@ -17,6 +17,8 @@ namespace Vidly.Models
         public bool IsSubscribedToNewsletter { get; set; }
         public MembershipType MembershipType { get; set; }
         public byte MembershipTypeId { get; set; }
+
+        [Min18YearsIfAMember]
         public DateTime? Birthdate { get; set; }
     }
 }
