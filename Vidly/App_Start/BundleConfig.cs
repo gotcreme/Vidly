19fdfeb3a0247d80886ae6dc60dd5eaa13939bad
Volodymyr Dykun <Volodymyr Dykun@DESKTOP-DM79T2C>
2017-02-08 12:06:23 +0200

    styles added

--- Vidly/App_Start/BundleConfig.cs
+++ Vidly/App_Start/BundleConfig.cs
@@ -31,6 +31,7 @@ namespace Vidly
                       "~/Content/datatables/css/datatables.bootstrap.css",
                       "~/Content/typeahead.css",
                       "~/Content/toastr.css",
+                      "~/Content/bootstrap-theme.css",
                       "~/Content/site.css"));
         }
     }
